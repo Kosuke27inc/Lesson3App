0c25dd19556a027acb5abf754e6ad1a708c8700e
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-05-30 21:44:48 +0900

    修正！  - MainActivity.ktからLog.dを削除して二重表示を無くした。  - 不要なログをコメントアウト  - コンストラクタの定義方法を変更（プライマリコンストラクタに修正）

--- app/src/main/java/jp/techacademy/kosuke/miyazaki/kotlinlog/Animal.kt
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/kotlinlog/Animal.kt
@@ -1,15 +1,8 @@
 package jp.techacademy.kosuke.miyazaki.kotlinlog
 
-abstract class Animal {
-    // プロパティ
-    var name: String
-    var age: Int
-
-    // 引数付きコンストラクタ
-    constructor(name: String, age: Int) {
-        this.name = name
-        this.age = age
-    }
+abstract class Animal //プライマリコンストラクタ
+    (var name: String = "aaa" , var age: Int
+) {
 
     abstract fun say()
 }
--- app/src/main/java/jp/techacademy/kosuke/miyazaki/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/kotlinlog/Human.kt
@@ -4,7 +4,7 @@ import android.util.Log
 open class Human(name: String, age: Int, var hobby: String): Animal(name, age), Thinkable {
 
     override fun say() {
-        Log.d("kotliintest", "私の名前は" + this.name + "です。年は" + this.age + "歳です。" )
+        Log.d("kotlintest", "私の名前は" + this.name + "です。年は" + this.age + "歳です。" )
     }
 
     override fun think() {
